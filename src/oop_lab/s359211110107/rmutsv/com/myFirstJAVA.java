b78dec8bb0a7a349392b776e20a773a58ade0acf
unknown <narathip6007@gmail>
2017-11-17 11:15:01 +0700

    Initial commit

---
+++ src/oop_lab/s359211110107/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,10 @@
+package oop_lab.s359211110107.rmutsv.com;
+
+public class myFirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm JANE");
+        System.out.println("Information Sytem");
+        System.out.println("Faculty of Managment Technology");
+        System.out.println("RMUTSV");
+    }//main
+}//class
