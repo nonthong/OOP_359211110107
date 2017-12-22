b847c87ea572d31931011c2c43360215e58b3d8a
LAB_COM-MT_1 <narathip6007@gmail.com>
2017-12-22 09:18:08 +0700

    Into Array

--- src/ooplab5/myArray.java
+++ src/ooplab5/myArray.java
@@ -12,6 +12,17 @@ public class myArray {
         myList[3 - 1] =500;
         myList[4] =myList[1 + 2] *10;
         showArry(myList);
+        String[] msg = new  String[3];
+        System.out.println(msg[0]);
+        double[] number = new double[5];
+        System.out.println(number[0]);
+        char[] c = new char[5];
+        System.out.println(c[0]);
+
+        Integer[] num = new  Integer[10];
+        System.out.println(num[0]);
+        Double[] num2 = new Double[10];
+        System.out.println(num2[0]);
 
     }//main
 
