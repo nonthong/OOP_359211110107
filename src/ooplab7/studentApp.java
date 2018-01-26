6860e1ae3e319edd8d82fe1a127612e4a3e26281
LAB_COM-MT_1 <narathip6007@gmail.com>
2018-01-26 10:08:48 +0700

    Class and Object: Access to instant variable

--- src/ooplab7/studentApp.java
+++ src/ooplab7/studentApp.java
@@ -21,7 +21,19 @@ public class studentApp {
         System.out.println(s2.toString());
 
         student s3 = new student();
-        s3 = inputData(s3);
+        //s3 = inputData(s3);
+        //System.out.println(s3.toString());
+
+        System.out.println(s2.getId();
+        s2.setId("004");
+        System.out.println(s2.getId();
+        s2.group = "223";
+        System.out.println(s2.group);
+        System.out.println(student.age);
+        student.age =20;
+        s2.age = 30;
+        System.out.println(student.age);
+        System.out.println(s2.age);
     }//main
 
     private static student inputData(student s) {
