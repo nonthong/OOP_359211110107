0ff71f70360874f85042ef8371e9d6537aa228e7
LAB_COM-MT_1 <narathip6007@gmail.com>
2018-02-02 11:01:32 +0700

    cat

---
+++ src/ooplap8/Shapeapp.java
@@ -0,0 +1,9 @@
+package ooplap8;
+
+public class Shapeapp {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(20,30);
+
+        System.out.println(rectangle.findArea());
+    }
+}
