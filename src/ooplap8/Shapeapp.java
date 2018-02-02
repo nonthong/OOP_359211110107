4d1d28b67321c533646cf2163af6c29e83317329
nonthong <33741991+nonthong@users.noreply.github.com>
2018-02-02 11:14:00 +0700

    Delete Shapeapp.java

--- src/ooplap8/Shapeapp.java
+++
@@ -1,9 +0,0 @@
-package ooplap8;
-
-public class Shapeapp {
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(20,30);
-
-        System.out.println(rectangle.findArea());
-    }
-}
